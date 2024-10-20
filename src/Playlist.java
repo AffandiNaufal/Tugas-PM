8c6eea8890039e1b3df29d580786e3a5d6acb851
AffandiNaufal <affandinaufal03@gmail.com>
2024-10-20 22:22:32 +0700

    git remote add origin https://github.com/AffandiNaufal/Tugas-PM.git git branch -M main git push -u origin main

---
+++ src/Playlist.java
@@ -0,0 +1,54 @@
+public class Playlist {
+
+    private class Song {
+        private String title;
+        private Song next;
+        public Song(String title) {
+            this.title = title;
+            this.next = null;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public Song getNext() {
+            return next;
+        }
+        public void setNext(Song next) {
+            this.next = next;
+        }
+    }
+    private Song head;
+
+    public Playlist() {
+        this.head = null;
+    }
+
+    public void addSong(String title) {
+        Song newSong = new Song(title);
+        if (head == null) {
+            head = newSong;
+        } else {
+            Song current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newSong);
+        }
+    }
+    public void displayPlaylist() {
+        Song current = head;
+        while (current != null) {
+            System.out.println(current.getTitle());
+            current = current.getNext();
+        }
+    }
+    public static void main(String[] args) {
+        Playlist playlist = new Playlist();
+        playlist.addSong("Satu Bulan");
+        playlist.addSong("Masing Masing");
+        playlist.addSong("Selamat Tinggal");
+
+        System.out.println("Playlist:");
+        playlist.displayPlaylist();
+    }
+}
